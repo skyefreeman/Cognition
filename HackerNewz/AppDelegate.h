f72ea67e7e3c13526ac00ca463e079a81e243802
Skye <gskye@aol.com>
2015-06-24 21:33:07 -0400

    Initial Commit

---
+++ HackerNewz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerNewz
+//
+//  Created by Skye on 6/24/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackerNewz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackerNewz
+//
+//  Created by Skye on 6/24/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
